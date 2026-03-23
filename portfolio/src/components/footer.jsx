216bd55eed74c7536af968f3faf8781a626ae1b8
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2026-03-23 17:25:05 -0300

    Feat: Project Started

---
+++ portfolio/src/components/footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h2>Informações de Contato:</h2>
+            </div>
+            <div>
+                <h3>Telefone: 11940829316</h3>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default footer
---
+++ portfolio/src/components/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+        <div>
+        <h3>Sobre</h3>
+        </div>
+        <div>
+        <h3>Linguagens</h3>
+        </div>
+        <div>
+        <h3>Projetos</h3>
+        </div>
+        <div>
+        
+        </div>
+    </div>
+  )
+}
+
+export default header
