4ab0e720e266e94568ade47d71cacce5270576cb
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2026-04-10 16:28:10 -0300

    Feat: added a temporary Photo

--- portfolio/src/components/Intro.jsx
+++ portfolio/src/components/Intro.jsx
@@ -1,34 +1,25 @@
 import React from 'react'
-
+import './intro.css'
 const Intro = () => {
   return (
      <div
-        class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-start"
+        class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-start" id='body-intro'
       >
         <div class="col-md-6 p-lg-5 mx-auto my-5">
           <h1 class="display-3 fw-bold">Arthur Bonvecchio Batista</h1>
-          <h3 class="fw-normal text-muted mb-3">
+          <h3 class="fw-normal  mb-3" id='dev'>
             Desenvolvedor Full-Stack
           </h3>
-          <div class="d-flex gap-3 justify-content-start lead fw-normal">
-            <a class="icon-link" href="#">
-              Learn more
-              <svg class="bi" aria-hidden="true">
-                <use xlink:href="#chevron-right"></use>
-              </svg>
-            </a>
-            <a class="icon-link" href="#">
-              Buy
-              <svg class="bi" aria-hidden="true">
-                <use xlink:href="#chevron-right"></use>
-              </svg>
-            </a>
-          </div>
         </div>
         <div class="product-device shadow-sm d-none d-md-block"></div>
         <div
           class="product-device product-device-2 shadow-sm d-none d-md-block"
         ></div>
+
+        <div id='profile'>
+          <img src="/foto.png" alt="" />
+        </div>
+
       </div>
   )
 }
--- portfolio/src/components/projects.jsx
+++ portfolio/src/components/projects.jsx
@@ -5,9 +5,35 @@ import './projects.css'
 const projects = () => {
   return (
     <div class="album py-5 px-4">
-        <h2 class="pb-2 border-bottom" id='project-title'>Formação Acadêmica</h2>
+        <h2 class="pb-2 border-bottom" id='project-title'>Meus Projetos</h2>
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+           {/*PRIMEIRO PROJETO*/}
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg aria-label="Placeholder: Thumbnail" class="bd-placeholder-img card-img-top" 
+              height="225" preserveAspectRatio="xMidYMid slice" role="img" width="100%" 
+              xmlns="http://www.w3.org/2000/svg">
+                <rect width="100%" height="100%" fill="#55595c"></rect>
+                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+              </svg>
+              <div class="card-body">
+                <h4>PORTFOLIO</h4>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in 
+                  to additional content. This content is a little bit longer.</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+ {/*--------------------------------------------------------------------------------------------------------------------*/}
+
+
+
 
           {/*PRIMEIRO PROJETO*/}
           <div class="col">
