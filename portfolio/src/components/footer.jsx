e69796e2647d911c8349a66538ec453c42b32fb1
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2026-05-07 16:55:29 -0300

    Feat: partially finished the footer

--- portfolio/src/components/footer.jsx
+++ portfolio/src/components/footer.jsx
@@ -5,46 +5,46 @@ const Footer = () => {
   return (
     <footer className="footer d-flex flex-wrap justify-content-between align-items-center py-3 px-4">
       
-      <div className="align-items-start" id='footer-texts'>
+      <div className="align-items-start reveal" id='footer-texts'>
         <img src="/mylogo.png" alt="" id='footer-logo' />
         <h5>Desenvolvedor apaixonado por criar experiencias modernas, rápidas e intuitivas</h5>
       </div>
 
-      <div id='social-medias'>
+      <div id='social-medias' className='reveal'>
 
 
-        <h1>Vamos Nos Conectar !</h1>
+        <h1 className='reveal'>Vamos Nos Conectar !</h1>
 
         <ul className="nav justify-content-end list-unstyled d-flex">
-          <li className="ms-3">
+          <li className="ms-3 reveal">
             <img src="/gitlogo.webp" alt="" />
           </li>
 
-          <li className="ms-3">
-            <img src="/linkedas.webp" alt="" />
+          <li className="ms-3 reveal">
+            <img className='reveal' src="/linkedas.webp" alt="" />
           </li>
         </ul>
 
         
       </div>
 
-      <div id='line-footer'>
-        <img src="line-footer.png" alt="" />
+      <div id='line-footer' className='reveal'>
+        <img className='reveal' src="line-footer.png" alt="" />
       </div>
 
-      <div id='final-footer'>
+      <div id='final-footer' className='reveal'>
           <h3>Feito com 💙 e muito código!</h3>
         </div>
 
 
-    <div className="footer-line"></div>
+    <div className="footer-line reveal"></div>
     
-    <div id='footer-symbol'>
+    <div id='footer-symbol' className='reveal'>
       <img src="/footersymbol.png" alt="" />
     </div>
 
     <div id='copyright'>
-    <h2>@ 2026 ABB Todos os Direitos Reservados</h2>
+    <h2 className='reveal'>@ 2026 ABB Todos os Direitos Reservados</h2>
     </div>
 
     </footer>
