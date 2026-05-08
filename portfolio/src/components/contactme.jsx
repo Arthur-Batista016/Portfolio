1440938316024319829f7a5fdd1c4ebe610a4f42
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2026-05-08 14:25:20 -0300

    Feat: added email send

--- portfolio/src/components/contactme.jsx
+++ portfolio/src/components/contactme.jsx
@@ -1,50 +1,136 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contactme.css'
-const contactme = () => {
+import emailjs from '@emailjs/browser'
+
+const Contactme = () => {
+
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm(
+      'service_bxnimxs',
+      'template_fyhbg5s',
+      form.current,
+      'nZsbmbWhcdR-MHsIS'
+    )
+    .then(() => {
+
+      alert('Mensagem enviada com sucesso! 🚀')
+
+      // limpa formulário
+      form.current.reset()
+
+    })
+    .catch((error) => {
+
+      console.log(error)
+
+      alert('Erro ao enviar mensagem ❌')
+
+    })
+  }
+
   return (
     <div className="container d-flex justify-content-end" id='items-contact'>
-      <div className="col-md-7 col-lg-8">
-        <h1 className="mb-3 reveal">Vamos Conversar !</h1>
 
-        <form>
+      <div className="col-md-7 col-lg-8">
+
+        <h1 className="mb-3 reveal">
+          Vamos Conversar !
+        </h1>
+
+        <form ref={form} onSubmit={sendEmail}>
+
           <div className="row g-3">
 
             <div className="col-sm-6 reveal">
-              <label className="form-label">Nome</label>
-              <input type="text" className="form-control inputs" required />
+
+              <label className="form-label">
+                Nome
+              </label>
+
+              <input
+                type="text"
+                name="user_name"
+                className="form-control inputs"
+                required
+              />
+
             </div>
 
             <div className="col-sm-6 reveal">
-              <label className="form-label reveal">Telefone</label>
-              <input type="tel" className="form-control inputs" placeholder="Ex: (11) 99999-9999" required />
+
+              <label className="form-label">
+                Telefone
+              </label>
+
+              <input
+                type="tel"
+                name="user_phone"
+                className="form-control inputs"
+                placeholder="Ex: (11) 99999-9999"
+                required
+              />
+
             </div>
 
             <div className="col-12 reveal">
-              <label className="form-label">Email</label>
-              <input type="email" className="form-control inputs" placeholder="Ex: you@example.com" required />
+
+              <label className="form-label">
+                Email
+              </label>
+
+              <input
+                type="email"
+                name="user_email"
+                className="form-control inputs"
+                placeholder="Ex: you@example.com"
+                required
+              />
+
             </div>
 
             <div className="col-12 reveal">
-              <label className="form-label">Mensagem</label>
-              <textarea   className="form-control inputs"
-                          id='mensagem'
-                          rows="1"
-                          onInput={(e) => {
-                            e.target.style.height = "auto";
-                            e.target.style.height = e.target.scrollHeight + "px";
-                          }}
-                          required></textarea>
+
+              <label className="form-label">
+                Mensagem
+              </label>
+
+              <textarea
+                name="message"
+                className="form-control inputs"
+                id='mensagem'
+                rows="1"
+                onInput={(e) => {
+                  e.target.style.height = "auto";
+                  e.target.style.height = e.target.scrollHeight + "px";
+                }}
+                required
+              ></textarea>
+
             </div>
 
             <div className="col-12 reveal">
-              <button className="w-100 btn btn-primary btn-lg ">Enviar</button>
+
+              <button
+                type="submit"
+                className="w-100 btn btn-primary btn-lg"
+              >
+                Enviar
+              </button>
+
             </div>
 
           </div>
+
         </form>
+
       </div>
+
     </div>
   )
 }
 
-export default contactme
+export default Contactme
